170b7e3c3b80bab990abc73795c2fa49de528c1f
Vanlsh <iparan800@ukr.net>
2023-04-12 16:35:58 +0300

    search

--- src/page/Search.jsx
+++ src/page/Search.jsx
@@ -1,11 +1,104 @@
-import React from "react";
-import { TextField, Autocomplete, Paper, Box, Stack } from "@mui/material";
+import React, { useState } from "react";
+import {
+  TextField,
+  Paper,
+  Box,
+  Stack,
+  Button,
+  Typography,
+} from "@mui/material";
 
 export const Search = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Name:", name);
+    console.log("Email:", email);
+    console.log("Message:", message);
+  };
   return (
-    <Stack direction="row" spacing={2}>
-      <Paper>1</Paper>
-      <Paper>2</Paper>
+    <Stack
+      direction={{ xs: "column", sm: "row" }}
+      spacing={{ xs: 0, sm: 2 }}
+      sx={{ flexGrow: 1, padding: "20px 10px" }}
+    >
+      <Paper
+        sx={{
+          flexGrow: 1,
+          mb: { xs: 2, sm: 0 },
+          padding: "5px",
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <form style={{ width: "90%" }} onSubmit={handleSubmit}>
+          <Typography>User 1</Typography>
+          <Box display="flex" flexDirection="column" maxWidth="500px">
+            <TextField
+              label="Title"
+              variant="outlined"
+              margin="normal"
+              value={name}
+              onChange={(event) => setName(event.target.value)}
+            />
+            <TextField
+              label="Author"
+              variant="outlined"
+              margin="normal"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+            />
+            <TextField
+              label="Genre"
+              variant="outlined"
+              margin="normal"
+              multiline
+              rows={4}
+              value={message}
+              onChange={(event) => setMessage(event.target.value)}
+            />
+            <Button type="submit" variant="contained" color="primary">
+              Submit
+            </Button>
+          </Box>
+        </form>
+      </Paper>
+      <Paper sx={{ flexGrow: 1, padding: "10px" }}>
+        <form style={{ width: "90%" }} onSubmit={handleSubmit}>
+          <Typography>User 2</Typography>
+          <Box display="flex" flexDirection="column" maxWidth="500px">
+            <TextField
+              label="Name"
+              variant="outlined"
+              margin="normal"
+              value={name}
+              onChange={(event) => setName(event.target.value)}
+            />
+            <TextField
+              label="Email"
+              variant="outlined"
+              margin="normal"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+            />
+            <TextField
+              label="Message"
+              variant="outlined"
+              margin="normal"
+              multiline
+              rows={4}
+              value={message}
+              onChange={(event) => setMessage(event.target.value)}
+            />
+            <Button type="submit" variant="contained" color="primary">
+              Submit
+            </Button>
+          </Box>
+        </form>
+      </Paper>
     </Stack>
   );
 };
